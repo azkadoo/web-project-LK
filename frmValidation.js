4a4e0b218e51839048d2a53b1d19110719f3a0b5
wdy04 <widyaayuu04@gmail.com>
2024-10-10 15:09:37 +0700

    frmValidation.js

--- frmValidation.js
+++ frmValidation.js
@@ -1,1 +1,15 @@
-// memeriksa apakah semua input sudah terisi atau apakah email valid.
+//<input type="email" id="email" placeholder="Email" required>
+//required untuk pengguna tidak bisa submit
+//lebih lengkap ada di individu
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+        const sign_up_btn = document.querySelector("#sign-up-btn");
+        const container = document.querySelector(".container");
+
+        sign_up_btn.addEventListener("click", () => {
+            container.classList.add("sign-up-mode");
+        });
+
+        sign_in_btn.addEventListener("click", () => {
+            container.classList.remove("sign-up-mode");
+        });
