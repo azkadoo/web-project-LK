473174222d3528d44dfb4f3863d692b1f2af7a5f
Mega82 <megawidiastuti6@gmail.com>
2024-10-10 10:30:56 +0700

    Update btnHide.js

--- btnHide.js
+++ btnHide.js
@@ -1,1 +1,19 @@
 // Tambahkan tombol yang bisa menampilkan atau menyembunyikan elemen di halaman, seperti menyembunyikan deskripsi fitur atau gambar testimonial.
+
+ <button id="toggleButton">Sembunyikan Review</button>
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById("toggleButton");
+    const reviewSection = document.querySelector(".review-section");
+
+    toggleButton.addEventListener("click", function() {
+        // Memeriksa apakah section saat ini terlihat atau tidak
+        if (reviewSection.classList.contains("hidden")) {
+            reviewSection.classList.remove("hidden")
+            toggleButton.textContent = "Sembunyikan Review"
+        } else {
+            reviewSection.classList.add("hidden")
+            toggleButton.textContent = "Tampilkan Review"
+        }
+    });
+});
