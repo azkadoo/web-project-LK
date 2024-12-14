b7eb9c699a3224e023a0781d31f46b71b3b04de5
Usamah Abdillah Hanif <55451537+usamah25@users.noreply.github.com>
2024-12-14 10:04:42 +0700

    Delete LensaKata_new/LensaKata directory

--- LensaKata_new/LensaKata/LensaKata_App/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(ReviewCard)
-admin.site.register(Story)
-admin.site.register(GameSession)
-admin.site.register(UserProgress)
